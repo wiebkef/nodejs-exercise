fd905ead3902af73083baf2a67ae55950264c735
Wiebke <wiebke.feldt@gmail.com>
2023-02-28 13:11:52 +0100

    added orders/:id route

--- orders.js
+++ orders.js
@@ -17,4 +17,22 @@ ordersRouter.get("/", (req, res) => {
     });
 });
 
+ordersRouter.get("/:id", (req, res) => {
+  const id = req.params.id;
+  pool
+    .query("SELECT * FROM ORDERS WHERE id=$1;", [id])
+    .then((data) => {
+      if (data.rowCount === 0) {
+        res.status(404).json({ message: "Order not found" });
+      } else {
+        res.json(data.rows[0]);
+      }
+    })
+    .catch((e) => {
+      res
+        .status(500)
+        .json({ message: "An error occured while fetching order." });
+    });
+});
+
 module.exports = ordersRouter;
